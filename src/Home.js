795d1c7b8d9bee46aca4b6950a036ca8903217c3
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-10-31 17:48:46 +0300

    added preloader

--- src/Home.js
+++ src/Home.js
@@ -7,23 +7,25 @@ const Home = () => {
 
 
     const [posts, setPosts] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
 
 
     useEffect(() => {
-        console.log("useEffect run");
+        console.log("useEffect run")
 
         fetch("http://localhost:8000/post").then(res => {
             return res.json()
         }).then(data => {
             console.log(data)
             setPosts(data)
+            setIsLoading(false)
         })
     }, [])
 
     return (
         <div className="home">
+            {isLoading && <h3>Loading...</h3>}
             { posts && <BlogList posts={posts} /> }
-
         </div>
 
     );
