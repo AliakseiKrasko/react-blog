6ffaeca7a3d7ad85b9cdb1557fda15a8011ac9e6
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-10-31 14:33:31 +0300

    added BlogList

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const BlogList = ({blog: posts}) => {
+    return (
+        <div className="blog">
+
+            {posts.map((post) => (
+                <div className="post-preview" key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.author}</p>
+                    <button className="btn-delete">Delete</button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import BlogList from "./BlogList";
 
 const Home = () => {
+
+    const blog = [
+        {
+            title: "My First Blog",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fuga nisi cum earum odio assumenda provident laborum quisquam ipsum, eaque accusantium maiores incidunt atque porro dicta dolores ad soluta modi?",
+            author: "John Doe",
+            id: 1
+        },
+        {
+            title: "Second Post",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fuga nisi cum earum odio assumenda provident laborum quisquam ipsum, eaque accusantium maiores incidunt atque porro dicta dolores ad soluta modi?",
+            author: "Mary Jane",
+            id: 2
+        },
+        {
+            title: "Third Post",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fuga nisi cum earum odio assumenda provident laborum quisquam ipsum, eaque accusantium maiores incidunt atque porro dicta dolores ad soluta modi?",
+            author: "Tom Soyer",
+            id: 3
+        }
+    ]
+
     return (
-        <div className="blog">
-            <div className="post-preview">
-                <h2>Title</h2>
-                <p>author</p>
-                <button className="btn-delete">Delete</button>
-            </div>
-
-            <div className="post-preview">
-                <h2>Title</h2>
-                <p>author</p>
-                <button className="btn-delete">Delete</button>
-            </div>
-
-            <div className="post-preview">
-                <h2>Title</h2>
-                <p>author</p>
-                <button className="btn-delete">Delete</button>
-            </div>
+        <div className="Home">
+            <BlogList blog={blog}/>
         </div>
+
     );
 };
 
